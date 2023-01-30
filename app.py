26de0128c53f9d54399978a7a96d2fd5fefd9eec
Juho <JuhoPaananen@users.noreply.github.com>
2023-01-30 17:17:35 +0200

    Create first version

---
+++ app.py
@@ -0,0 +1,11 @@
+from os import getenv
+from flask import Flask
+from flask import redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    admin BOOLEAN DEFAULT FALSE,
+    insider BOOLEAN
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    secret BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    category_id INT NOT NULL REFERENCES categories(id),
+    title TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    topic_id INT NOT NULL REFERENCES topics(id),
+    content TEXT NOT NULL,
+    sent_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    message_id INT NOT NULL REFERENCES messages(id),
+    vote INT DEFAULT 1
+);
