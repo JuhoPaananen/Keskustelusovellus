9747f9c543f782f58b2802bdb7eb5d461e840402
Juho <JuhoPaananen@users.noreply.github.com>
2023-01-31 15:44:58 +0200

    Create first version of login

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from os import getenv
 from flask import Flask
-from flask import redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+
+import routes
 
